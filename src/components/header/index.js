83c9fb9627e92d1f7304a01227c7072887fb60ee
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-03-22 22:48:28 +0530

    add footer & header component

---
+++ src/components/header/index.js
@@ -0,0 +1,26 @@
+import './header.css';
+
+export default function Header(){
+    return(
+    <header className="header">
+      <div className="header-items">
+        <div className="nav-brand">
+          <a className="btn-link" href="/index.html">Midori</a>
+        </div>
+          <div className="input-icon icons-left">
+            <span className="material-icons">search</span> 
+            <input className="input" type="text" id="search-bar" name="search-bar" placeholder="search"/> 
+          </div>
+          <div className="sub-menu"> 
+            <button className="btn btn-cta">Login</button>
+            <div className="header-icon">
+              <a className="btn-link" href="/mycart/mycart.html"><span className="material-icons">shopping_cart</span></a>                   
+            </div>
+            <div className="header-icon"> 
+              <a className="btn-link" href="/mywishlist/wishlist.html"><span className="material-icons">favorite_border</span></a>                
+            </div>                         
+         </div> 
+       </div>
+    </header>  
+ )
+}
