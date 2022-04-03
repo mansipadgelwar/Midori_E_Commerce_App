b7ed07d38aa19999001ceb93d7fd45915278de16
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2022-04-03 21:11:13 +0530

    add categories to backend

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -14,7 +14,8 @@ export const products = [
     discountedPrice: 299,
     discount: "25% OFF",
     categoryName: "indoor",
-    rating: 3
+    rating: 3,
+    outOfStock: true
   },
   {
     _id: uuid(),
@@ -24,7 +25,8 @@ export const products = [
     discountedPrice: 249,
     discount: "29% OFF",
     categoryName: "indoor",
-    rating: 5
+    rating: 5,
+    outOfStock: false
   },
   {
     _id: uuid(),
@@ -34,7 +36,8 @@ export const products = [
     discountedPrice: 349,
     discount: "13% OFF",
     categoryName: "indoor",
-    rating: 2
+    rating: 2,
+    outOfStock: false
   },
   {
     _id: uuid(),
@@ -44,7 +47,8 @@ export const products = [
     discountedPrice: 995,
     discount: "0",
     categoryName: "climbers",
-    rating: 3
+    rating: 3,
+    outOfStock: false
   },
   {
     _id: uuid(),
@@ -54,7 +58,8 @@ export const products = [
     discountedPrice: 349,
     discount: "13% OFF",
     categoryName: "cacti",
-    rating: 4
+    rating: 4,
+    outOfStock: false
   },
   {
     _id: uuid(),
@@ -64,7 +69,8 @@ export const products = [
     discountedPrice: 399,
     discount: "20% OFF",
     categoryName: "cacti",
-    rating: 2
+    rating: 2,
+    outOfStock: true
   },
   {
     _id: uuid(),
@@ -74,6 +80,29 @@ export const products = [
     discountedPrice: 399,
     discount: "20% OFF",
     categoryName: "cacti",
-    rating: 5
+    rating: 5,
+    outOfStock: false
+  },
+  {
+    _id: uuid(),
+    name: "Rugmini Plant",
+    image: "",
+    actualPrice: 749,
+    discountedPrice: 599,
+    discount: "20% OFF",
+    categoryName: "flowering",
+    rating: 5,
+    outOfStock: false
+  },
+  {
+    _id: uuid(),
+    name: "White Orchid",
+    image: "",
+    actualPrice: 1299,
+    discountedPrice: 999,
+    discount: "23% OFF",
+    categoryName: "flowering",
+    rating: 5,
+    outOfStock: true
   }
 ];
